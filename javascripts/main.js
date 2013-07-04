5d492ad5a49985761b409f687678dce6882a008d
ufologist <klsforever@gmail.com>
2013-07-04 10:15:15 -0700

    对于中文标题锚点构造不正确

--- javascripts/main.js
+++ javascripts/main.js
@@ -14,8 +14,8 @@ $(window).resize(sectionHeight);
 
 $(document).ready(function(){
   $("section h1, section h2").each(function(){
-    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
-    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
+    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).find('a').attr('name') + "'>" + $(this).text() + "</a></li>");
+    $(this).attr("id", $(this).find('a').attr('name'));
     $("nav ul li:first-child a").parent().addClass("active");
   });
   
